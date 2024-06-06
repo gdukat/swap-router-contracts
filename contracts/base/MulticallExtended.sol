83cda0780a5eac1d5dcf7b80ac360c1bb204b560
137gdukat <137gdukat@gmail.com>
2024-06-05 22:55:56 -0700

    pragma solidity >=0.7.6;

--- contracts/base/MulticallExtended.sol
+++ contracts/base/MulticallExtended.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity =0.7.6;
+pragma solidity >=0.7.6;
 pragma abicoder v2;
 
 import '@uniswap/v3-periphery/contracts/base/Multicall.sol';
@@ -11,24 +11,18 @@ import '../base/PeripheryValidationExtended.sol';
 /// @notice Enables calling multiple methods in a single call to the contract
 abstract contract MulticallExtended is IMulticallExtended, Multicall, PeripheryValidationExtended {
     /// @inheritdoc IMulticallExtended
-    function multicall(uint256 deadline, bytes[] calldata data)
-        external
-        payable
-        override
-        checkDeadline(deadline)
-        returns (bytes[] memory)
-    {
+    function multicall(
+        uint256 deadline,
+        bytes[] calldata data
+    ) external payable override checkDeadline(deadline) returns (bytes[] memory) {
         return multicall(data);
     }
 
     /// @inheritdoc IMulticallExtended
-    function multicall(bytes32 previousBlockhash, bytes[] calldata data)
-        external
-        payable
-        override
-        checkPreviousBlockhash(previousBlockhash)
-        returns (bytes[] memory)
-    {
+    function multicall(
+        bytes32 previousBlockhash,
+        bytes[] calldata data
+    ) external payable override checkPreviousBlockhash(previousBlockhash) returns (bytes[] memory) {
         return multicall(data);
     }
 }
